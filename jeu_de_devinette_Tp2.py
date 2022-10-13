0cb61c2ca1dff97411e24044746e587c6f9d5c77
Daquipilj <112640262+Daquipilj@users.noreply.github.com>
2022-10-13 08:30:15 -0400

    Update jeu_de_devinette_Tp2.py

--- jeu_de_devinette_Tp2.py
+++ jeu_de_devinette_Tp2.py
@@ -2,32 +2,30 @@ import random
 
 def jeu_devinette():
 
-    print("J'ai choisi un nombre entre 0 et 1000. Deviner le nombre")
+    borne_minimal = int(input("Choisissez une borne minimal"))
+    borne_maximal = int(input("Choisissez une borne maximal"))
 
-    nombre_random = random.randint(0, 1000)
-    print(nombre_random)
+    nombre_random = random.randint(borne_minimal, borne_maximal)
     essai = int(input("Entrez votre essai:"))
     nb_essai = 0
 
 
-
     while essai != nombre_random:
-        nb_essai += 1
+            nb_essai += 1
 
-        if essai < nombre_random:
-            essai = int(input("le nombre est plus grand "))
-
-
-        elif essai > nombre_random:
-             essai = int(input("le nombre est plus petit"))
-
-        if essai == nombre_random:
-            print ("bravo  vous avez devinez mon chiffre en" + nb_essai + "essai")
-            rejouer = int(input("voulez vous rejouer? oui/non"))
-            if rejouer: "non"
-                break
+            if essai < nombre_random:
+                essai = int(input("le nombre est plus grand "))
 
 
+            elif essai > nombre_random:
+                 essai = int(input("le nombre est plus petit"))
 
+    while essai == nombre_random:
+        print ("bravo  vous avez devinez mon chiffre en " + str(nb_essai) + " essai")
+        rejouer = str(input("voulez vous rejouer? oui/non"))
+        if rejouer == "non":
+            exit()
+        if rejouer =="oui":
+            jeu_devinette()
 
 jeu_devinette()
